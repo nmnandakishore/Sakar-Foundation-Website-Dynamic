22acb98008ff760b7f0ac1ae65215657eecc3953
Nandakishore N M <nmnandakishore@gmail.com>
2023-05-08 13:14:18 +0530

    Content updated for beyond schools page

--- pages/beyond-schools.tsx
+++ pages/beyond-schools.tsx
@@ -27,20 +27,21 @@ const emptyComponent: React.FC<IHomePageProps> = ({ topics = null }) => {
     console.log({ topicItems });
 
     return (
-        <>            
+        <>
             <div className="page-header relative z-0 pt-14 pb-0 bg-teal-700 mb-10 text-slate-400 bg-fixed bg-center bg-cover bg-blend-color-burn shadow-gray-500 drop-shadow-lg ">
                 <div className="container">
                     <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-10 pt-10 pb-32">
                         <div>
                             <img src="/img/beyond-schools-hero.png" alt="img" />
                         </div>
-                        <div className="lg:py-24">
+                        <div className="lg:py-16">
                             <div className="inline-block bg-slate-700 text-white rounded-full py-2 px-4">Inclusive Education</div>
-                            <h1 className="py-4 text-6xl text-white">Educating children <br /> for tomorrow</h1>
-                            <a className="inline-block text-white bg-humans py-2 px-4 bg-secondary" href="#">Join Now</a>
+                            <h1 className="pt-4 py-2 text-4xl text-white">A Holistic Lens <br /> to Steer Your Life with Progress</h1>
+                            <h2 className="pt-4 py-2 text-light text-white">We bring you Life and the Real World, Beyond the Bindings of your Coursebook</h2>
+                            <a className="inline-block text-white bg-humans py-2 mt-4 px-4 bg-secondary" href="#">Join Now</a>
                         </div>
                     </div>
-                </div>                
+                </div>
             </div>
 
             <div className="section relative mt-16 min-h-20 sm:-mt-32 z-100">
@@ -94,34 +95,51 @@ const emptyComponent: React.FC<IHomePageProps> = ({ topics = null }) => {
             <div className="section mt-28">
                 <div className="container py-20">
                     <div className="grid grid-cols-1 sm:grid-cols-2 sm:gap-10 pt-10">
-                        <div className='' >
+                        <div className='sm: py-20' >
                             <img src="https://solverwp.com/demo/html/edumint/assets/img/about/3.png" alt="" />
                         </div>
                         <div className='sm:py-20'>
-                            <div className="heading text-primary" data-id="2">Beyond Schools</div>
+                            <div className="heading text-primary" data-id="2">A New Outlook Towards Knowledge - Beyond Schools</div>
                             <p className="my-5" data-id="2">
-                                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                                We believe that knowledge and wisdom do not confine to the boundaries of your classroom! The practicalities and challenges of the natural world, impart the real essence of learning. With this unique approach promising a learning drive, Beyond Schools focuses on the holistic growth and development of young and curious minds to stay ahead and ready in both diverse and adverse life situations!.
                             </p>
 
-                            <div className="grid grid-cols-1 sm:grid-cols-5 sm:gap-10 pt-10">
-                                <div className='py-3 text-center'><i className="text-6xl fa-solid fa-user-graduate text-teal-600"></i></div>
+                            <div className="grid grid-cols-1 sm:grid-cols-5 sm:gap-10 pt-5">
+                                <div className='py-6 text-center'>
+                                    {/* <span className="fa-stack text- text-4xl align-sub"> */}
+                                        {/* <i className="fa-solid fa-square text-teal-600 fa-stack-2x"></i> */}
+                                        {/* <i className="fa-solid fa-filter fa-inverse fa-stack-1x text-white"></i> */}
+                                        <i className="text-5xl fa-solid fa-chalkboard-user text-teal-600"></i>
+                                    {/* </span> */}
+                                </div>
                                 <div className='sm:col-span-4'>
-                                    <div className="text-primary text-2xl">Sub Heading 1</div>
+                                    {/* <div className="text-primary text-2xl">Sub Heading 1</div> */}
                                     <p>
-                                        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium.
+                                        Curated Teachings on a Range of Essential Topics. From mental health to Financial Knowledge, we have got you covered in all aspects and phases of life!
                                     </p>
                                 </div>
                             </div>
 
-                            <div className="grid grid-cols-1 sm:grid-cols-5 sm:gap-10 pt-10">
-                                <div className='py-6 text-center'><i className="text-6xl fa-solid fa-award text-teal-600"></i></div>
+                            <div className="grid grid-cols-1 sm:grid-cols-5 sm:gap-10 pt-5">
+                                <div className='py-6 text-center'><i className="text-6xl fa-solid fa-person-chalkboard text-teal-600"></i></div>
                                 <div className='sm:col-span-4'>
-                                    <div className="text-primary text-2xl">Sub Heading 2</div>
+                                    {/* <div className="text-primary text-2xl">Sub Heading  2</div> */}
                                     <p>
-                                        totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                                        Led by Experts in the Field! We bring together like-minded individuals who deliver the best aspects on the topics and leave a kindle of change in your mind!                                    </p>
+                                </div>
+                            </div>
+
+                            <div className="grid grid-cols-1 sm:grid-cols-5 sm:gap-10 pt-5">
+                                <div className='py-6 text-center'><i className="text-6xl fa-solid fa-handshake-angle text-teal-600"></i></div>
+                                <div className='sm:col-span-4'>
+                                    {/* <div className="text-primary text-2xl">Sub Heading  2</div> */}
+                                    <p>
+                                        Easy access to all information at a finger&apos;s touch! If you want to seek information on more topics, feel free to reach out to us with your suggestions!
                                     </p>
                                 </div>
                             </div>
+
+
                         </div>
                     </div>
                 </div>
